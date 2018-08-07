d651359fb7c09d4ed9c712fb2abc892059428955
Musa Jahun <msjahun@live.com>
2018-08-07 10:23:53 +0300

    did some refactoring

--- Reading_XML_using_linq_c_sharp/Program.cs
+++ Reading_XML_using_linq_c_sharp/Program.cs
@@ -13,22 +13,32 @@ namespace Reading_XML_using_linq_c_sharp
         {
 
             XDocument xdoc = XDocument.Load("..\\..\\..\\database\\products.xml");
-            //without condition
-            //xdoc.Descendants("product").Select(p => new
-            //{
-            //    id = p.Attribute("id").Value,
-            //    name = p.Element("name").Value,
-            //    price = p.Element("price").Value,
-            //    currency = p.Element("price").Attribute("currency").Value
-            //}).ToList().ForEach(p =>
-            //{
-            //    Console.WriteLine("Id: " + p.id);
-            //    Console.WriteLine("Name: " + p.name);
-            //    Console.WriteLine("Price: "+ p.price);
-            //    Console.WriteLine("currency: "+ p.currency);
-            //    Console.WriteLine("");
-            //});
 
+            readingXMLFileWithCondition(xdoc);
+            readingXMLFileWithoutCondition(xdoc);
+            Console.ReadLine();
+
+        }
+        public static void readingXMLFileWithoutCondition(XDocument xdoc)
+        {
+
+            xdoc.Descendants("product").Select(p => new
+            {
+                id = p.Attribute("id").Value,
+                name = p.Element("name").Value,
+                price = p.Element("price").Value,
+                currency = p.Element("price").Attribute("currency").Value
+            }).ToList().ForEach(p =>
+            {
+                Console.WriteLine("Id: " + p.id);
+                Console.WriteLine("Name: " + p.name);
+                Console.WriteLine("Price: " + p.price);
+                Console.WriteLine("currency: " + p.currency);
+                Console.WriteLine("");
+            });
+        }
+        public static void readingXMLFileWithCondition(XDocument xdoc)
+        {
             //with condition
             xdoc.Descendants("product").Where(p => Convert.ToInt32(p.Element("price").Value) > 1000)
                 .Select(p => new
@@ -38,14 +48,17 @@ namespace Reading_XML_using_linq_c_sharp
                     price = p.Element("price").Value,
                     currency = p.Element("price").Attribute("currency").Value
                 }).ToList().ForEach(p =>
-            {
-                Console.WriteLine("Id: " + p.id);
-                Console.WriteLine("Name: " + p.name);
-                Console.WriteLine("Price: " + p.price);
-                Console.WriteLine("currency: " + p.currency);
-                Console.WriteLine("");
-            });
-            Console.ReadLine();
+                {
+                    Console.WriteLine("Id: " + p.id);
+                    Console.WriteLine("Name: " + p.name);
+                    Console.WriteLine("Price: " + p.price);
+                    Console.WriteLine("currency: " + p.currency);
+                    Console.WriteLine("");
+                });
+          
         }
+
+
     }
+
 }
